46702c31098d41816ad9ae3107b0a8fb8a3d8da4
Justin Kruskie <justin@jkruskie.com>
2023-03-28 18:11:53 -0400

    Python Example of Auth and Leak Report Post

---
+++ authentication.py
@@ -0,0 +1,65 @@
+import requests
+import json
+import yaml
+
+# Function to read the YAML configuration file
+def read_config_file(file_path):
+    with open(file_path, 'r') as file:
+        config_data = yaml.safe_load(file)
+    return config_data
+
+# Read the config.yml file
+config_file_path = 'config.yml'
+config_data = read_config_file(config_file_path)
+
+# Get the monitor mac address
+mac_address = config_data['monitor']['mac_address']
+
+# Get the location guid
+location_guid = config_data['location']['guid']
+
+# Root api url
+ROOT_URL = 'https://tankmonitorapi.dumbdevelopers.com/api'
+
+# Authentication Route
+AUTHENTICATION_ROUTE = '/monitor/authenticate'
+
+# Authentication url
+AUTHENTICATION_URL = ROOT_URL + AUTHENTICATION_ROUTE
+
+# Headers
+HEADERS = {
+    'Content-Type': 'application/json',
+    'User-Agent': 'TankMonitorAPI/1.0.0'
+}
+
+# POST request to authenticate using the monitor mac address and location guid
+post_data = {
+    'macAddress': mac_address,
+    'locationGuid': location_guid
+}
+
+# Print the post data
+print(post_data)
+
+response = requests.post(
+    AUTHENTICATION_URL, 
+    json=post_data,  # Use the json parameter to pass the post_data as JSON
+    headers=HEADERS
+)
+
+# Get the response data
+response_data = response.json()
+
+# Check if the response status code is 200
+if response.status_code == 200:
+    # Print the response data
+    print(response_data)
+    # Save the token to the config.yml file
+    config_data['access_token'] = response_data['accessToken']
+    # Write the config.yml file
+    with open(config_file_path, 'w') as file:
+        yaml.dump(config_data, file)
+else:
+    # Print the error message
+    print(response_data['message'])
---
+++ leakreport.py
@@ -0,0 +1,51 @@
+import requests  # Import the requests library
+import json
+import yaml
+
+# Function to read the YAML configuration file
+def read_config_file(file_path):
+    with open(file_path, 'r') as file:
+        config_data = yaml.safe_load(file)
+    return config_data
+
+# Read the config.yml file
+config_file_path = 'config.yml'
+config_data = read_config_file(config_file_path)
+
+# Get the monitor guid
+monitor_guid = config_data['monitor']['guid']
+
+# Root api url
+ROOT_URL = 'https://tankmonitorapi.dumbdevelopers.com/api'
+
+# URL to send the leak report
+URL = ROOT_URL + '/Monitors/' + monitor_guid + '/LeakReport'
+
+# Print the monitor guid
+print(monitor_guid)
+
+# Headers without Content-Type since requests library will set it automatically
+HEADERS = {
+    'User-Agent': 'TankMonitorAPI/1.0.0',
+    'Authorization': 'Bearer ' + config_data['access_token']
+}
+
+# Use 'with' statement to ensure the file is closed after reading
+with open('leakdata.txt', 'rb') as file:
+    # POST request to send the leak report
+    response = requests.post(
+        URL,
+        headers=HEADERS,
+        files={'file': file}  # Send the file as 'multipart/form-data'
+    )
+
+# Get the response data
+response_data = response.json()
+
+# Check if the response status code is 200
+if response.status_code == 200:
+    # Print the response data
+    print(response_data)
+else:
+    # Print the error message
+    print(response_data['message'])
